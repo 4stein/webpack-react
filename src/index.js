59adaa8c39a53d3c447c978b53c3464fb6f6b42c
mdoruchenko <doruchenko.m.i@gmail.com>
2022-04-29 18:00:01 +0300

    fix react version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles/index.sass';
 import App from './App';
 
-// root
-const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<App/>);
+// ReactDOM
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
+
 // hot
 if (module.hot) {
   module.hot.accept()
 };
+
