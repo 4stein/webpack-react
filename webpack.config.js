b96f530d3efbaf5eddfe3a039c0ca70603e61eca
mdoruchenko <doruchenko.m.i@gmail.com>
2022-04-29 22:08:14 +0300

    add soure map

--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,35 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 const config = {
 	mode: "development",
 	target: "web",
 
-	entry: "./src/index.js",
-
+	entry: {
+		bundle: path.join(__dirname, "src/index.js"),
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "[name][contenthash].js",
+		assetModuleFilename: "images/[fullhash][ext][query]",
+		// clean: true,
+	},
+	devtool: "source-map",
 	devServer: {
   	static: {
       directory: path.join(__dirname, "dist"),
     },
-    
     compress: true,
-    port: 3000,
+    port: 3001,
+    open: true,
     hot: true,
   },
 
 	module: {
 		rules: [
-			{
-        test: /\.[jt]sx?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [["@babel/preset-env", { "useBuiltIns": "usage", "corejs": 3, "targets": "defaults" }], "@babel/preset-react"]
-          }
-        }
-      },
 			{
 				test: /\.s[ac]ss$/i,
 				use: [
@@ -46,13 +45,23 @@ const config = {
 				],
 			},
 			{
+        test: /\.[jt]sx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { "useBuiltIns": "usage", "corejs": 3, "targets": "defaults" }], "@babel/preset-react"]
+          }
+        }
+      },
+			{
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
-        test: /\.(svg|png|gif|jpg)$/,
-        // type: "asset",  // "asset/inline",
+        test: /\.(svg|png|gif|jpe?g)$/,
+        type: "asset/resource", // "asset", // "asset/inline",
         exclude: /fonts/,
         loader: 'file-loader'
       },
@@ -67,6 +76,7 @@ const config = {
 		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin({ filename: "main.[fullhash].css" }),
 		new HtmlWebpackPlugin({
+			title: "Project App",
 			template: "./src/index.html",
 			favicon: "./src/images/react-img.jpg"
 		}),
@@ -75,19 +85,16 @@ const config = {
 	resolve: {
 		extensions: [".js", ".jsx", ".tsx", ".ts"],
 	},
-
-	devtool: "source-map",
-  output: {
-  	filename: "bundle.[fullhash].js",
-		path: path.join(__dirname, "dist"),
-		assetModuleFilename: "images/[fullhash][ext][query]",
-	},
 };
 
 if (currentTask == "build") {
   config.mode = "production";
   config.target = ['web', 'es5'];
-  config.plugins.push(new CleanWebpackPlugin(), new WebpackManifestPlugin());
+  config.plugins.push(
+  	new CleanWebpackPlugin(), 
+  	new WebpackManifestPlugin(),
+  	// new BundleAnalyzerPlugin()
+  );
 }
 
 module.exports = config;
