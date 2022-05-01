897b3ba601372cdb5a5f40521206ac2d5e3de5d5
mdoruchenko <doruchenko.m.i@gmail.com>
2022-05-01 19:26:56 +0300

    svg fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
-const currentTask = process.env.npm_lifecycle_event
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-
 const config = {
-	mode: "development",
-	target: "web",
+  mode: "development",
+  target: "web",
 
-	entry: {
-		bundle: path.join(__dirname, "src/index.js"),
-	},
-	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "[name][contenthash].js",
-		assetModuleFilename: "images/[contenthash][ext][query]",
-		// clean: true,
-	},
-	devtool: "source-map",
-	devServer: {
-  	static: {
+  entry: {
+    bundle: path.join(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    assetModuleFilename: "images/[contenthash][ext][query]",
+    // clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
       directory: path.join(__dirname, "dist"),
     },
     compress: true,
@@ -31,72 +30,78 @@ const config = {
     hot: true,
   },
 
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					currentTask == "start" ? 
-					"style-loader" :
-					{
-						loader: MiniCssExtractPlugin.loader, 
-						options: { publicPath: "" },
-					},
-					"css-loader", 
-					"sass-loader"
-				],
-			},
-			{
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          currentTask == "start"
+            ? "style-loader"
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: { publicPath: "" },
+              },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
         test: /\.[jt]sx?$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: [["@babel/preset-env", { "useBuiltIns": "usage", "corejs": 3, "targets": "defaults" }], "@babel/preset-react"]
-          }
-        }
+            presets: [
+              [
+                "@babel/preset-env",
+                { useBuiltIns: "usage", corejs: 3, targets: "defaults" },
+              ],
+              "@babel/preset-react",
+            ],
+          },
+        },
       },
-			{
+      {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpe?g|gif)$/i,
-    	type: 'asset/resource',
+        type: "asset/resource",
         //type: "asset/resource", // "asset", // "asset/inline",
-        // exclude: /fonts/,
+        exclude: /fonts/,
         // loader: 'file-loader'
       },
       {
-        test: /\.(ttf|eot|woff|svg|woff2)$/,
-        loader: "file-loader"
+        test: /\.(ttf|eot|woff|woff2)$/,
+        loader: "file-loader",
       },
-		]
-	},
+    ],
+  },
 
-	plugins: [
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin({ filename: "main.[fullhash].css" }),
-		new HtmlWebpackPlugin({
-			title: "Project App",
-			template: "./src/index.html",
-			favicon: "./src/images/react-img.jpg"
-		}),
-	],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "main.[fullhash].css" }),
+    new HtmlWebpackPlugin({
+      title: "Project App",
+      template: "./src/index.html",
+      favicon: "./src/images/react-img.jpg",
+    }),
+  ],
 
-	resolve: {
-		extensions: [".js", ".jsx", ".tsx", ".ts"],
-	},
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts"],
+  },
 };
 
 if (currentTask == "build") {
   config.mode = "production";
-  config.target = ['web', 'es5'];
+  config.target = ["web", "es5"];
   config.plugins.push(
-  	new CleanWebpackPlugin(), 
-  	new WebpackManifestPlugin(),
-  	// new BundleAnalyzerPlugin()
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin()
+    // new BundleAnalyzerPlugin()
   );
 }
 
